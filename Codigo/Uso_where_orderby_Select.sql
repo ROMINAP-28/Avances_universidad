7f3fc36b3d84eb47c6e3a8a6491613d7ac8647f7
ROMINAP-28 <rominanadielka2006@gmail.com>
2025-08-28 08:56:08 -0500

    carpeta codigos

---
+++ Codigo/Uso_where_orderby_Select.sql
@@ -0,0 +1,19 @@
+-- SELECT
+
+SELECT Nombres,Direccion 
+FROM Mantenimiento_Clientes
+
+-- WHERE 
+
+SELECT * FROM Mantenimiento_Vehiculo 
+WHERE color = 'Rojo';
+
+SELECT * FROM Mantenimiento_Vehiculo
+WHERE cantidad_puertas = 2;
+
+
+-- ORDER BY
+
+SELECT * FROM Mantenimiento_Vehiculo
+ORDER BY Idcodigo DESC;
+
