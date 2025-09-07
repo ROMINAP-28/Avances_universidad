c019fd10744d0c94be7d7cbf40d4f7ff2ad56191
ROMINAP-28 <rominanadielka2006@gmail.com>
2025-09-07 12:17:40 -0500

    base de datos

--- Codigo/innerjoin.sql
+++ Codigo/innerjoin.sql
@@ -9,3 +9,6 @@ JOIN Mantenimiento_Clientes ON Mantenimiento_Vehiculo.Idcodigo = Mantenimiento_C
 SELECT placa,Nombres,color FROM Mantenimiento_Vehiculo LEFT JOIN Mantenimiento_Clientes
 ON Mantenimiento_Vehiculo.Idcodigo = Mantenimiento_Clientes.Codigo
 
+SELECT celular,dueño
+FROM Mantenimiento_Clientes,Mantenimiento_Vehiculo
+WHERE Mantenimiento_Vehiculo.Idcodigo = Mantenimiento_Clientes.Codigo
